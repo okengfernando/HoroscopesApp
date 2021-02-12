a17fa839ecdfda75225d2d580020f8232ae46cd1
fernando <okengfernando@github.com>
2021-02-12 20:59:35 +0300

    added unit tests

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+import daily_horoscope
+
+
+  
+class Test_Horoscope_Methods(unittest.TestCase): 
+      
+    def setUp(self): 
+        pass
+
+    def test_check_sign(self):
+        self.assertTrue(type(daily_horoscope.check_sign(13,12)) == str)
+
+    def test_date_selector(self):
+        self.assertTrue(daily_horoscope.date_selector() == 'yesterday' or 'today' or 'tomorrow')
+    
+    def test_Horoscope_dipslay(self):
+        self.assertTrue(type(daily_horoscope.horoscope(9,'today')) == str)
+    
+    
+  
+if __name__ == '__main__': 
+    unittest.main() 
