99867621c233c8027fb1840538d530706df779df
fernando <okengfernando@github.com>
2021-06-03 18:47:10 +0300

    restructured code

--- daily_horoscope.py
+++ daily_horoscope.py
@@ -78,7 +78,13 @@ def horoscope(zodiac_sign: int, day: str) -> str:
         soup = BeautifulSoup(requests.get(url).content, "html.parser")
     except:
         return "Internet Connection Error, Check connection and Try Again!!"
-    return soup.find("div", class_="main-horoscope").p.text
+
+    message = soup.find("div", class_="main-horoscope").p.text 
+
+    print(" ")
+    print("****************************************************MESSAGE***************************************")
+    return message
+    print("**************************************************************************************************")
 
 
 def date_selector():
